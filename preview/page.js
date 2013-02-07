9739945e21a410087d3b9e664babe7cfdd180818
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-02-07 09:16:44 -0800

    Published at Thu Feb 07 2013 12:16:29 GMT-0500 (EST)

--- preview/page.js
+++ preview/page.js
@@ -1,10 +1,19 @@
 $(function() {
   
   data = JSON.parse(BL.getContentForPreview().content[0].data);
+  
+  $('button').on('click', function() {
+    BL.authorizeTwitter();
+  })
+  
   if (BL.twitterAuthenticated()) {
+    $('#twee').show()
     BL.tweet(data.tweetText, true, data.oncePerSession, function() {
       $('#tweet').text('You just tweeted: ' + data.tweetText);
     })
+  }
+  else {
+    $('button').show()
   }
     
   BL.previewReady(); // this is what tells the preview page to display this component - must be called
